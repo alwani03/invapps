0e06ccc5e4ab2e94bc30e58e004e4e9a3837243d
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-06-11 07:28:38 +0700

    first commit

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Created By Muhammad 2023 by <a href="https://www.instagram.com/_muuhh/" target="_blank">Instagram</a></span>
+		</div>
+	</div>
+</footer>
