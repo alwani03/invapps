8e69b26bd96aee672029da3074049859aa9c2833
muuhhh <muhammad.kjc@gmail.com>
2023-06-18 16:56:01 +0700

    ganti username

--- application/controllers/Pengeluaran.php
+++ application/controllers/Pengeluaran.php
@@ -2,8 +2,10 @@
 
 use Dompdf\Dompdf;
 
-class Pengeluaran extends CI_Controller{
-	public function __construct(){
+class Pengeluaran extends CI_Controller
+{
+	public function __construct()
+	{
 		parent::__construct();
 		date_default_timezone_set('Asia/Jakarta');
 		$this->data['aktif'] = 'pengeluaran';
@@ -13,7 +15,8 @@ class Pengeluaran extends CI_Controller{
 		$this->load->model('M_detail_keluar', 'm_detail_keluar');
 	}
 
-	public function index(){
+	public function index()
+	{
 		$this->data['title'] = 'Pembebanan';
 		$this->data['all_pengeluaran'] = $this->m_pengeluaran->lihat();
 		$this->data['no'] = 1;
@@ -21,7 +24,8 @@ class Pengeluaran extends CI_Controller{
 		$this->load->view('pengeluaran/lihat', $this->data);
 	}
 
-	public function tambah(){
+	public function tambah()
+	{
 		$this->data['title'] = 'Pembebanan';
 		$this->data['all_barang'] = $this->m_barang->lihat_data_masuk();
 		$this->data['all_customer'] = $this->m_customer->lihat_cst();
@@ -29,7 +33,8 @@ class Pengeluaran extends CI_Controller{
 		$this->load->view('pengeluaran/tambah', $this->data);
 	}
 
-	public function proses_tambah(){
+	public function proses_tambah()
+	{
 		$jumlah_barang_keluar = count($this->input->post('nama_barang_hidden'));
 
 		$data_keluar = [
@@ -40,11 +45,11 @@ class Pengeluaran extends CI_Controller{
 			'nama_petugas' 	 => $this->input->post('nama_petugas'),
 		];
 
-		
+
 
 		$data_detail_keluar = [];
 
-		for($i = 0; $i < $jumlah_barang_keluar; $i++){
+		for ($i = 0; $i < $jumlah_barang_keluar; $i++) {
 			array_push($data_detail_keluar, ['no_keluar' => $this->input->post('no_keluar')]);
 			$data_detail_keluar[$i]['nama_barang'] 	= $this->input->post('nama_barang_hidden')[$i];
 			$data_detail_keluar[$i]['jumlah'] 		= $this->input->post('jumlah_hidden')[$i];
@@ -54,8 +59,8 @@ class Pengeluaran extends CI_Controller{
 			$this->m_pengeluaran->kurang($data_detail_keluar[$i]['nama_barang'], $data_detail_keluar[$i]['jumlah']);
 		}
 
-		if($this->m_pengeluaran->tambah($data_keluar) && $this->m_detail_keluar->tambah($data_detail_keluar)){
-			for ($i=0; $i < $jumlah_barang_keluar ; $i++) { 
+		if ($this->m_pengeluaran->tambah($data_keluar) && $this->m_detail_keluar->tambah($data_detail_keluar)) {
+			for ($i = 0; $i < $jumlah_barang_keluar; $i++) {
 				$this->m_barang->min_stok($data_detail_keluar[$i]['jumlah'], $data_detail_keluar[$i]['nama_barang']) or die('gagal min stok');
 			}
 			$this->session->set_flashdata('success', 'Invoice <strong>Pengeluaran</strong> Berhasil Dibuat!');
@@ -63,7 +68,8 @@ class Pengeluaran extends CI_Controller{
 		}
 	}
 
-	public function detail($no_keluar){
+	public function detail($no_keluar)
+	{
 		$this->data['title'] = 'Detail Pengeluaran';
 		$this->data['pengeluaran'] = $this->m_pengeluaran->lihat_no_keluar($no_keluar);
 		$this->data['all_detail_keluar'] = $this->m_detail_keluar->lihat_no_keluar($no_keluar);
@@ -72,8 +78,9 @@ class Pengeluaran extends CI_Controller{
 		$this->load->view('pengeluaran/detail', $this->data);
 	}
 
-	public function hapus($no_keluar){
-		if($this->m_pengeluaran->hapus($no_keluar) && $this->m_detail_keluar->hapus($no_keluar)){
+	public function hapus($no_keluar)
+	{
+		if ($this->m_pengeluaran->hapus($no_keluar) && $this->m_detail_keluar->hapus($no_keluar)) {
 			$this->session->set_flashdata('success', 'Invoice Pengeluaran <strong>Berhasil</strong> Dihapus!');
 			redirect('pengeluaran');
 		} else {
@@ -82,24 +89,27 @@ class Pengeluaran extends CI_Controller{
 		}
 	}
 
-	public function get_all_barang(){
+	public function get_all_barang()
+	{
 
 		$data 	   = $this->m_barang->lihat_nama_barang($_POST['nama_barang']);
 		$datastock = $this->m_barang->lihat_nama_barang_masuk($_POST['nama_barang']);
 
-		if(isset($datastock->jumlah)){
+		if (isset($datastock->jumlah)) {
 			$data->stok = $datastock->jumlah;
-		}else{
+		} else {
 			$data->stok = 0;
 		}
 		echo json_encode($data);
 	}
 
-	public function keranjang_barang(){
+	public function keranjang_barang()
+	{
 		$this->load->view('pengeluaran/keranjang');
 	}
 
-	public function export(){
+	public function export()
+	{
 		$dompdf = new Dompdf();
 		// $this->data['perusahaan'] = $this->m_usaha->lihat();
 		$this->data['all_pengeluaran'] = $this->m_pengeluaran->lihat();
@@ -113,7 +123,8 @@ class Pengeluaran extends CI_Controller{
 		$dompdf->stream('Laporan Data Pengeluaran Tanggal ' . date('d F Y'), array("Attachment" => false));
 	}
 
-	public function export_detail($no_keluar){
+	public function export_detail($no_keluar)
+	{
 		$dompdf = new Dompdf();
 		// $this->data['perusahaan'] = $this->m_usaha->lihat();
 		$this->data['pengeluaran'] = $this->m_pengeluaran->lihat_no_keluar($no_keluar);
