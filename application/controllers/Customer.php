4f02a5a3a0c0a31d7142939b4a36602ebad4da6d
muuhhh <muhammad.kjc@gmail.com>
2023-06-18 17:00:48 +0700

    ganti judul

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -2,24 +2,28 @@
 
 use Dompdf\Dompdf;
 
-class Customer extends CI_Controller{
-	public function __construct(){
+class Customer extends CI_Controller
+{
+	public function __construct()
+	{
 		parent::__construct();
-		if($this->session->login['role'] != 'petugas' && $this->session->login['role'] != 'admin') redirect();
+		if ($this->session->login['role'] != 'petugas' && $this->session->login['role'] != 'admin') redirect();
 		$this->load->model('M_customer', 'm_customer');
 		$this->data['aktif'] = 'customer';
 	}
 
-	public function index(){
-		$this->data['title'] = 'Data Customer';
+	public function index()
+	{
+		$this->data['title'] = 'Data Pengambil';
 		$this->data['all_customer'] = $this->m_customer->lihat();
 		$this->data['no'] = 1;
 
 		$this->load->view('customer/lihat', $this->data);
 	}
 
-	public function tambah(){
-		if ($this->session->login['role'] == 'petugas'){
+	public function tambah()
+	{
+		if ($this->session->login['role'] == 'petugas') {
 			$this->session->set_flashdata('error', 'Tambah data hanya untuk admin!');
 			redirect('dashboard');
 		}
@@ -29,8 +33,9 @@ class Customer extends CI_Controller{
 		$this->load->view('customer/tambah', $this->data);
 	}
 
-	public function proses_tambah(){
-		if ($this->session->login['role'] == 'petugas'){
+	public function proses_tambah()
+	{
+		if ($this->session->login['role'] == 'petugas') {
 			$this->session->set_flashdata('error', 'Tambah data hanya untuk admin!');
 			redirect('dashboard');
 		}
@@ -43,7 +48,7 @@ class Customer extends CI_Controller{
 			'alamat' => $this->input->post('alamat'),
 		];
 
-		if($this->m_customer->tambah($data)){
+		if ($this->m_customer->tambah($data)) {
 			$this->session->set_flashdata('success', 'Data Customer <strong>Berhasil</strong> Ditambahkan!');
 			redirect('customer');
 		} else {
@@ -52,8 +57,9 @@ class Customer extends CI_Controller{
 		}
 	}
 
-	public function ubah($kode){
-		if ($this->session->login['role'] == 'petugas'){
+	public function ubah($kode)
+	{
+		if ($this->session->login['role'] == 'petugas') {
 			$this->session->set_flashdata('error', 'Ubah data hanya untuk admin!');
 			redirect('dashboard');
 		}
@@ -64,8 +70,9 @@ class Customer extends CI_Controller{
 		$this->load->view('customer/ubah', $this->data);
 	}
 
-	public function proses_ubah($kode){
-		if ($this->session->login['role'] == 'petugas'){
+	public function proses_ubah($kode)
+	{
+		if ($this->session->login['role'] == 'petugas') {
 			$this->session->set_flashdata('error', 'Ubah data hanya untuk admin!');
 			redirect('dashboard');
 		}
@@ -78,7 +85,7 @@ class Customer extends CI_Controller{
 			'alamat' => $this->input->post('alamat'),
 		];
 
-		if($this->m_customer->ubah($data, $kode)){
+		if ($this->m_customer->ubah($data, $kode)) {
 			$this->session->set_flashdata('success', 'Data Customer <strong>Berhasil</strong> Diubah!');
 			redirect('customer');
 		} else {
@@ -87,13 +94,14 @@ class Customer extends CI_Controller{
 		}
 	}
 
-	public function hapus($kode){
-		if ($this->session->login['role'] == 'petugas'){
+	public function hapus($kode)
+	{
+		if ($this->session->login['role'] == 'petugas') {
 			$this->session->set_flashdata('error', 'Hapus data hanya untuk admin!');
 			redirect('dashboard');
 		}
-		
-		if($this->m_customer->hapus($kode)){
+
+		if ($this->m_customer->hapus($kode)) {
 			$this->session->set_flashdata('success', 'Data Customer <strong>Berhasil</strong> Dihapus!');
 			redirect('customer');
 		} else {
@@ -102,7 +110,8 @@ class Customer extends CI_Controller{
 		}
 	}
 
-	public function export(){
+	public function export()
+	{
 		$dompdf = new Dompdf();
 		$this->data['all_customer'] = $this->m_customer->lihat();
 		$this->data['title'] = 'Laporan Data Customer';
